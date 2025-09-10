22e9dccf403b10c8ed5e6b25a84f630311a247ea
xxyyffyeah <33823764+xxyyffyeah@users.noreply.github.com>
2025-09-10 23:09:02 +0000

    Deploying to gh-pages from @ xxyyffyeah/xxyyffyeah.github.io@cb948d77eb34c7dec6b2e1bd73e8d81bc9e2c7a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "TA experiences",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
