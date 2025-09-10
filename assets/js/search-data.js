9897c25f42fd4520d85fb4e8b869291c9f2a810e
xxyyffyeah <33823764+xxyyffyeah@users.noreply.github.com>
2025-09-10 23:02:07 +0000

    Deploying to gh-pages from @ xxyyffyeah/xxyyffyeah.github.io@cbc57df1adcd09bb6b49e77c5a75245babeefa4d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/Yifan_Xu_Resume_phd.pdf";
           },
         },{id: "nav-teaching",
           title: "teaching",
