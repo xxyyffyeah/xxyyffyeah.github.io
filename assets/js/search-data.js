701dfc39cb78610a04533966767eb2677d50e1cf
xxyyffyeah <33823764+xxyyffyeah@users.noreply.github.com>
2025-09-11 03:36:47 +0000

    Deploying to gh-pages from @ xxyyffyeah/xxyyffyeah.github.io@7949b21c76f821e6131b8ce7d40d4252ddc183d8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
@@ -61,11 +61,11 @@ ninja.data = [{
           window.open("mailto:%78%78%79%79%66%66%79%65%61%68@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/xxyyffyeah", "_blank");
         },
       },{
         id: 'social-rss',
@@ -79,7 +79,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=JBxtlcoAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
